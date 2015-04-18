528a9e0b09b82e2baa6953cd0d648676fe66df64
frogOfPrincess <304793812@qq.com>
2015-04-18 23:26:25 +0800

    增加update函数

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -31,57 +31,6 @@ bool HelloWorld::init()
         return false;
     }
 
-
- /* 屏蔽原代码
-
-    CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
-    CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
-
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-                                        "CloseNormal.png",
-                                        "CloseSelected.png",
-                                        this,
-                                        menu_selector(HelloWorld::menuCloseCallback));
-    
-	pCloseItem->setPosition(ccp(origin.x + visibleSize.width - pCloseItem->getContentSize().width/2 ,
-                                origin.y + pCloseItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
-    pMenu->setPosition(CCPointZero);
-    this->addChild(pMenu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Arial", 24);
-    
-    // position the label on the center of the screen
-    pLabel->setPosition(ccp(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - pLabel->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(pLabel, 1);
-
-    // add "HelloWorld" splash screen"
-    CCSprite* pSprite = CCSprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    pSprite->setPosition(ccp(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(pSprite, 0);
-    
-*/
-
 	addStart();
 
 	funForStart();
@@ -171,19 +120,7 @@ bool HelloWorld::ccTouchBegan(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent
 	CCLOG("ccTouchBegan~~");
 	CCPoint touchPoint = pTouch->getLocation();
 	Block* pBlock = NULL ;
-	/*
 
-	Block* pBlock = blocks.getData( 2 ) ;
-	if ( pBlock->boundingBox().containsPoint(touchPoint))
-	{
-		CCLOG("contain~~!!");
-	}
-	else
-	{
-		//addEnd();
-	}
-
-	*/
 	
 	//遍历所有块
 	for (int i = 0 ; i<blocks.m_nCount ; ++i )
@@ -236,4 +173,9 @@ void HelloWorld::moveDown()
 		pBlock = blocks.getData( i );
 		pBlock->moveDown();
 	}
+}
+
+void HelloWorld::update( float t )
+{
+	CCLOG("void HelloWorld::update( float t )") ;
 }
